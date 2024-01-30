3985698af6f1be7829998714c4f7cb1fa4597daa
Kashyap Patel <kash@Kashyaps-MacBook-Pro.local>
2024-01-30 00:54:30 -0500

    Loading data from JSON file

--- server.js
+++ server.js
@@ -1,36 +1,33 @@
 // Budget API
 
-const express = require('express');
+const express = require("express");
+const fs = require("fs");
 const app = express();
 const port = 3000;
 
-app.use('/', express.static('public'));
+app.use("/", express.static("public"));
 
-const budget = {
-    myBudget: [
-        {
-            title: 'Eat out',
-            budget: 25
-        },
-        {
-            title: 'Rent',
-            budget: 375
-        },
-        {
-            title: 'Grocery',
-            budget: 110
-        }
-    ]
-};
+const budgetData = JSON.parse(fs.readFileSync("budget-data.json", "utf8"));
 
-app.get('/hello', (req, res) => {
-    res.send('Hello World!');
+app.get("/hello", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.get('/budget', (req, res) => {
-    res.json(budget);
+app.get("/budget", (req, res) => {
+  if (!budgetData) {
+    try {
+      const data = fs.readFileSync("budget-data.json", "utf8");
+      budgetData = JSON.parse(data);
+    } catch (err) {
+      console.error("Error reading JSON file:", err);
+      res.status(500).json({ error: "Internal Server Error" });
+      return;
+    }
+  }
+
+  res.json(budgetData);
 });
 
 app.listen(port, () => {
-    console.log(`API served at http://localhost:${port}`);
+  console.log(`API served at http://localhost:${port}`);
 });
