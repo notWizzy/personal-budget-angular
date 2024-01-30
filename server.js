b28c898ecd30225f0bb9b866ba8e6efdbff824a7
Kashyap Patel <106133901+notWizzy@users.noreply.github.com>
2024-01-30 00:31:51 -0500

    "Added server, Axios, and ChartJS"

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 // Budget API
 
 const express = require('express');
-const cors = require('cors');
 const app = express();
 const port = 3000;
 
-app.use(cors());
+app.use('/', express.static('public'));
 
 const budget = {
     myBudget: [
@@ -15,15 +14,18 @@ const budget = {
         },
         {
             title: 'Rent',
-            budget: 275
+            budget: 375
         },
         {
             title: 'Grocery',
             budget: 110
-        },
+        }
     ]
 };
 
+app.get('/hello', (req, res) => {
+    res.send('Hello World!');
+});
 
 app.get('/budget', (req, res) => {
     res.json(budget);
